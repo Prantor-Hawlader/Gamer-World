df9caa88b21d2d80fe9c36e55d9fd37d1543e25a
Prantor-Hawlader <prantor502@gmail.comm>
2023-05-01 21:15:09 +0600

    Displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -8,10 +8,11 @@ name : string,
 slug : string
 }
 export interface Game {
-  id: number;
-  name: string;
-  background_image : string;
-  parent_platforms : {platform : Platform}[]
+  id: number,
+  name: string,
+  background_image : string,
+  parent_platforms : {platform : Platform}[],
+  metacritic : number,
 }
 interface FecthGamesResponse {
   counts: number;
