953ef56bb9b1f355d4cfe717aee9d75972c42266
Prantor-Hawlader <prantor502@gmail.comm>
2023-05-01 22:46:35 +0600

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -22,22 +22,26 @@ interface FecthGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading,setLoading] = useState(false);
 
   const controller = new AbortController();
 
   useEffect(() => {
+    setLoading(true)
     apiClient
       .get<FecthGamesResponse>("/games", {signal : controller.signal})
-      .then((res) => setGames(res.data.results))
+      .then((res) => {setGames(res.data.results)
+                      setLoading(false)})
       .catch((err) => {
         if(err instanceof CanceledError) return;
-        setError(err.message)});
+        setError(err.message)
+      setLoading(false)});
 
 
       return () => controller.abort();
   }, []);
 
-  return { games, error } ;
+  return { games, error,isLoading } ;
 };
 
 export default useGames;
